cb03f218455b3061729fdf91db46ce35104337d6
SuperMonster <ilby0616@qq.com>
2024-06-10 21:04:47 +0800

    0.60

--- aParkingSystem/src/main/java/aParkingSystem/aParkingSystem.java
+++
@@ -1,9 +0,0 @@
-package aParkingSystem;
-
-import aParkingSystem.frame.LoginFrame;
-
-public class aParkingSystem {
-    public static void main(String[] args) {
-        new LoginFrame();
-    }
-}
